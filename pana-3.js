660a8c0f8885e5836283b6f2a82fcdfd94fa753c
Asharib Ali <102221198+AsharibAli@users.noreply.github.com>
2022-10-01 07:23:24 -0700

    Task completed
    Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.

---
+++ pana-3.js
@@ -0,0 +1,10 @@
+var fullName = "Asharib Ali";
+
+// Uppercase in javascript
+console.log(fullName.toUpperCase());
+
+// Lowercase in javascript
+console.log(fullName.toLowerCase());
+
+// Titlecase in javascript
+console.log(`${fullName.charAt(0).toUpperCase()} ${fullName.slice(1)}`);
