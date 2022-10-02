3ac407a1c23d0c85aed1d36546d6e9e4cd5254df
Asharib Ali <102221198+AsharibAli@users.noreply.github.com>
2022-10-02 03:15:18 -0700

    Task completed
    Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your message.

---
+++ pana-5.js
@@ -0,0 +1,5 @@
+var famous_person = `Albert Einstein`;
+var quote = `“A person who never made a mistake never tried anything new.”`;
+var message = `${famous_person} once said, ${quote}`;
+
+console.log(message);
