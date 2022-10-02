2af78dcb660dc282a772dbe4aa21e949fb8f6d9f
Asharib Ali <102221198+AsharibAli@users.noreply.github.com>
2022-10-01 23:23:09 -0700

    Task completed
    Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author. Your output should look something like the following, including the quotation marks:

---
+++ pana-4.js
@@ -0,0 +1,5 @@
+//Success Quote by Frank Sinatra
+const successQuote = `"The best revenge is massive success"`;
+const authorName = `"Frank Sinatra"`;
+
+console.log(`${authorName} said, ${successQuote}`);
