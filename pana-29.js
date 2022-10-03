f7be5d92776b4ecf425be9c4e1c46b35370bc6ef
Asharib Ali <102221198+AsharibAli@users.noreply.github.com>
2022-10-03 09:48:01 -0700

    Task completed
    Favorite Fruit: Make a array of your favorite fruits, and then write a series of independent if statements that check for certain fruits in your array.
    • Make a array of your three favorite fruits and call it favorite_fruits.
    • Write five if statements. Each should check whether a certain kind of fruit is in your array. If the fruit is in your array, the if block should print a statement, such as You really like bananas!

---
+++ pana-29.js
@@ -0,0 +1,18 @@
+var favorite_fruits = ["Banana" , "Orange", "Apple"];
+console.log("Favorite Fruits List", favorite_fruits);
+
+if(favorite_fruits.includes("Orange")){
+     console.log("I really like Oranges");
+}
+if(favorite_fruits.includes("PineApple")){
+    console.log("I love PineApples");
+}
+if(favorite_fruits.includes("Banana")){
+    console.log("I love Bananas");
+}
+if(favorite_fruits.includes("Grapes")){
+    console.log("I love Grapes");
+}
+if(favorite_fruits.includes("Apple")){
+    console.log("I love Apples");
+}
