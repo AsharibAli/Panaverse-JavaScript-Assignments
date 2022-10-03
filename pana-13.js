bec80f878f114101605dd3e7a5723f7403430c74
Asharib Ali <102221198+AsharibAli@users.noreply.github.com>
2022-10-02 20:37:44 -0700

    Task completed
    Your Own Array: Think of your favorite mode of transportation, such as a motorcycle or a car, and make a list that stores several examples. Use your list to print a series of statements about these items, such as “I would like to own a Honda motorcycle.”

---
+++ pana-13.js
@@ -0,0 +1,6 @@
+let cars = ["Alto", "Grande", "Civic"];
+let carsStatements = ["is affordable", "is faster", "is luxury"];
+
+for (let i in cars) {
+        console.log(`${cars[i]} ${carsStatements[i]}`)
+};
