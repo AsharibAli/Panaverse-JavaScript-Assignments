abca2526a713c46765fe655c0a68b477386d94ce
Asharib Ali <102221198+AsharibAli@users.noreply.github.com>
2022-10-03 00:38:08 -0700

    Task completed
    Seeing the World: Think of at least five places in the world you’d like to visit.
    
    • Store the locations in a array. Make sure the array is not in alphabetical order.
    • Print your array in its original order.
    • Print your array in alphabetical order without modifying the actual list.
    • Show that your array is still in its original order by printing it.
    • Print your array in reverse alphabetical order without changing the order of the original list.
    • Show that your array is still in its original order by printing it again.
    • Reverse the order of your list. Print the array to show that its order has changed.
    • Reverse the order of your list again. Print the list to show it’s back to its original order.
    • Sort your array so it’s stored in alphabetical order. Print the array to show that its order has been changed.
    • Sort to change your array so it’s stored in reverse alphabetical order. Print the list to show that its order has changed.

---
+++ pana-18.js
@@ -0,0 +1,19 @@
+var places = ["India", "Turkey", "Usa", "Nigeria", "Saudia"];
+console.log(places);
+
+var convertPlaces = places.slice().sort()
+console.log(convertPlaces);
+console.log(places);
+
+var convertPlacesReverse = convertPlaces.reverse()
+console.log(convertPlacesReverse);
+console.log(places);
+
+places.reverse();
+console.log(places);
+places.reverse()
+console.log(places);
+
+places.sort()
+console.log(places);
+console.log(places.reverse());
