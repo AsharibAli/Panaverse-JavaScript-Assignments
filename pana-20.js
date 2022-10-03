29390e6f34d857d0d615d1a05855fd8196df18dd
Asharib Ali <102221198+AsharibAli@users.noreply.github.com>
2022-10-03 00:48:34 -0700

    Task completed
    Think of something you could store in a array. For example, you could make a list of mountains, rivers, countries, cities, languages, or anything else you’d like. Write a program that creates a list containing these items.

---
+++ pana-20.js
@@ -0,0 +1,5 @@
+// var countries = ["India", "Canadia", "England", "USA"];
+// var cities = ["Nawabshah", "Lahore", "Faislabad", "Karachi"];
+
+var pakCities = new Array(1, "Lahore", "Faisalabad", "Karachi")
+console.log(pakCities);
