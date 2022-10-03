0e2a2e99110cb437d147dd95b37f943c5981efab
Asharib Ali <102221198+AsharibAli@users.noreply.github.com>
2022-10-03 00:20:12 -0700

    Task completed
    More Guests: You just found a bigger dinner table, so now more space is available. Think of three more guests to invite to dinner.
    • Start with your program from Exercise 15. Add a print statement to the end of your program informing people that you found a bigger dinner table.
    • Add one new guest to the beginning of your array.
    • Add one new guest to the middle of your array. • Use append() to add one new guest to the end of your list. • Print a new set of invitation messages, one for each person in your list.

--- pana-16.js
+++ pana-16.js
@@ -1,21 +1,21 @@
-var invitedFrens = ["Awais", "Shahnoor", "Samad"];
+var callFrens = ["Awais", "Shahnoor", "Samad"];
 var notInTheCity = "Shahnoor"
 
-function inviteFriends() {
-    invitedFrens.forEach((name, i) => {
+function callFriends() {
+    callFrens.forEach((name, i) => {
         if (name === notInTheCity) {
-            invitedFrens[i] = "Ayan"
+            callFrens[i] = "Ayan"
             console.log(`Hi ${name} i would like to dinner with you thats why i'm inviting you`)
         } else {
             console.log(`Hi ${name} i would like to dinner with you thats why i'm inviting you`)}
     })
 }
-inviteFriends()
-console.log("Hello frens i would like to do a biger dinner");
+callFriends()
+console.log("Hello guy's i would like to do a bigger dinner with you");
 
-invitedFrens.unshift("Ahmed");
-invitedFrens.splice(2, 0, "Murtaza");
-invitedFrens.push("Ibrahim");
+callFrens.unshift("Ahmed");
+callFrens.splice(2, 0, "Murtaza");
+callFrens.push("Ibrahim");
 
-inviteFriends()
-console.log(invitedFrens);
+callFrens()
+console.log(callFriends);
