17413f68c8bac2cd0694e15f7fc6e879c6875aef
Asharib Ali <102221198+AsharibAli@users.noreply.github.com>
2022-10-03 01:38:37 -0700

    Task completed
    More Conditional Tests: You don’t have to limit the number of tests you create to 10. If you want to try more comparisons, write more tests. Have at least one True and one False result for each of the following:
    • Tests for equality and inequality with strings
    • Tests using the lower case function
    • Numerical tests involving equality and inequality, greater than and less than, greater than or equal to, and less than or equal to
    • Tests using "and" and "or" operators
    • Test whether an item is in a array
    • Test whether an item is not in a array

---
+++ pana-23.js
@@ -0,0 +1,39 @@
+let car = 'Civic';
+let rakshaw = 'Sohrab';
+let rakshawModel = 2022;
+let rakshawSpeed = '40km';
+let rakshawTyre = '3Tyre';
+
+
+console.log("Is car == 'Civic'? I predict True.");
+console.log(car == 'Civic');
+
+console.log("Is rakshaw == 'Sohrab'? I predict True.");
+console.log(rakshaw == 'Sohrab');
+
+console.log("Is rakshawModel == 2022? I predict True.");
+console.log(rakshawModel == 2022);
+
+console.log("Is rakshaw != '100km'? I predict True.");
+console.log(rakshawSpeed != '100km');
+
+console.log("Is rakshawTyre == '3tyre'? I predict True.");
+console.log(rakshawTyre == '3tyre');
+
+console.log("Is car != 'Civic'? I predict False.");
+console.log(car != 'Civic');
+
+console.log("Is rakshaw != 'Sohrab'? I predict False.");
+console.log(rakshaw != 'Sohrab');
+
+console.log("Is rakshawModel == 2022? I predict True.");
+console.log(rakshawModel == 2022);
+
+console.log("Is rakshaw != '100km'? I predict True.");
+console.log(rakshawSpeed != '100km');
+
+console.log("Is rakshawTyre == '3tyre'? I predict True.");
+console.log(rakshawTyre == '3tyre');
+
+console.log("Is car == 'Civic'? I predict False.");
+console.log(car.toLowerCase() == 'Subaru'.toLowerCase());
