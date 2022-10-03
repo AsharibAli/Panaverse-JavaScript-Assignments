6996093a80dafa83781d42f27db3ed8a5a7753eb
Asharib Ali <102221198+AsharibAli@users.noreply.github.com>
2022-10-02 20:04:34 -0700

    Task completed
    Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. If you don’t have anything specific to write because your programs are too simple at this point, just add your name and the current date at the top of each program file. Then write one sentence describing what the program does.

---
+++ pana-10.js
@@ -0,0 +1,10 @@
+//Store a persons name in a variable and print a message to that person.let
+
+fullName = "Asharib Ali"
+console.log(`Hello ${fullName}, would you like to learn some Python today?`);
+
+// Famous Quote by Albert Einstein
+const quote = `"A person who never made a mistake never tried anything new."`;
+const authorName = `Albert Einstein`;
+
+console.log(`${authorName} once said, ${quote}`);
