f2a5e56ee4642792be111a19c5d2c3806dd69bc2
Asharib Ali <102221198+AsharibAli@users.noreply.github.com>
2022-10-03 03:18:38 -0700

    Task completed
    More Conditional Tests: You don’t have to limit the number of tests you create to 10. If you want to try more comparisons, write more tests. Have at least one True and one False result for each of the following:
    • Tests for equality and inequality with strings
    • Tests using the lower case function
    • Numerical tests involving equality and inequality, greater than and less than, greater than or equal to, and less than or equal to
    • Tests using "and" and "or" operators
    • Test whether an item is in a array
    • Test whether an item is not in a array=

---
+++ pana-24.js
@@ -0,0 +1,15 @@
+console.log(4 + 4 == 8) //true
+console.log(4 + 4 - 4 == 8) //false
+console.log(4 + 4 < 8) //false
+console.log(4 + 4 > 8) //false
+
+console.log(4 + 4 >= 8) //true
+console.log(4 + 4 <= 8) //true
+
+console.log(4 + 4 <= 8 && 6 + 6 == 10) //false
+console.log(4 + 4 <= 8 && 6 + 6 == 12) //true
+console.log(4 + 4 <= 8 || 5 + 5 == 8) //false
+
+const arr  = [1,2,4]
+console.log(arr.includes(2)) //true
+console.log(arr.includes(3)) //false
