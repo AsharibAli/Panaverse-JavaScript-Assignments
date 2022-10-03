f492cb8a16ce52dbbba7fd22fc4d9c747769505d
Asharib Ali <102221198+AsharibAli@users.noreply.github.com>
2022-10-03 09:35:27 -0700

    pana-27.js
    Alien Colors #3: Turn your if-else chain from Exercise 5-4 into an if-else chain.
    • If the alien is green, print a message that the player earned 5 points.
    • If the alien is yellow, print a message that the player earned 10 points.
    • If the alien is red, print a message that the player earned 15 points.

---
+++ pana-27.js
@@ -0,0 +1,13 @@
+let alien_color1 = "green";
+let alien_color2 = "red";
+let alien_color3 = "yellow";
+
+if(alien_color1 === "green") {
+    console.log("The player just earn 5 points");
+    if(alien_color2 === "red") {
+        console.log("The player earned 15 points");
+    }
+    if(alien_color3 === 'yellow') {
+        console.log("The player earned 10 points");
+    }
+};
