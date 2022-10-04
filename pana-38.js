905930e508480368c92f53cd55cdee0d2a109a86
Asharib Ali <102221198+AsharibAli@users.noreply.github.com>
2022-10-04 11:29:20 -0700

    Task completed
    Cities: Write a function called describe_city() that accepts the name of a city and its country. The function should print a simple sentence, such as Karachi is in Pakistan. Give the parameter for the country a default value. Call your function for three different cities, at least one of which is not in the default country.

---
+++ pana-38.js
@@ -0,0 +1,6 @@
+function describe_city(name, country = "Pakistan") {
+    console.log(`${name} is in ${country}`)
+}
+describe_city("islamabad");
+describe_city("gujrat", "india");
+describe_city("karachi");
