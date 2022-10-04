17df7cd81e21e5bb425dd1cc0731dfc1d49836a3
Asharib Ali <102221198+AsharibAli@users.noreply.github.com>
2022-10-03 20:21:25 -0700

    Task completed
    No Users: Add an if test to Exercise 28 to make sure the list of users is not empty.
    • If the list is empty, print the message We need to find some users!
    • Remove all of the usernames from your array, and make sure the correct message is printed.

---
+++ pana-31.js
@@ -0,0 +1,8 @@
+var users = ['Ali','Ahmed','Ibrahim','Yousaf','Bilal'];
+ while (users.length > 0) {
+    users.pop();
+}
+console.log(users.length);
+if ( users.length == 0) {
+    console.log("We need to find some users!");
+};
