4aa301a76a3e7c1c0bed6d2d246aa488215e447d
Asharib Ali <102221198+AsharibAli@users.noreply.github.com>
2022-10-04 11:30:47 -0700

    Task completed
    City Names: Write a function called city_country() that takes in the name of a city and its country. The function should return a string formatted like this:

---
+++ pana-39.js
@@ -0,0 +1,6 @@
+function city_country(name, country = "Pakistan") {
+    console.log(`${name}, ${country}`)
+}
+city_country("islamabad");
+city_country("gujrat", "india");
+city_country("karachi");
