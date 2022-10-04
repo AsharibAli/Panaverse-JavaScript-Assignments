0cccfa4385571f50595b35d89515cf2d129dca97
Asharib Ali <102221198+AsharibAli@users.noreply.github.com>
2022-10-04 11:34:53 -0700

    Task completed
    Magicians: Make a array of magician’s names. Pass the array to a function called show_magicians(), which prints the name of each magician in the array.

---
+++ pana-41.js
@@ -0,0 +1,9 @@
+let magiciansNames = ["eric", "san", "deck", "tim"];
+
+function show_magicians(magicians) {
+    magicians.forEach((mag) => {
+        console.log(mag)
+    })
+}
+
+show_magicians(magiciansNames);
