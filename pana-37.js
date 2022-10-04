04b9260aa9fbd62e5a4b64150d2a62e889b1f675
Asharib Ali <102221198+AsharibAli@users.noreply.github.com>
2022-10-04 11:28:01 -0700

    Task completed
    Large Shirts: Modify the make_shirt() function so that shirts are large by default with a message that reads I love JavaScript. Make a large shirt and a medium shirt with the default message, and a shirt of any size with a different message.

---
+++ pana-37.js
@@ -0,0 +1,7 @@
+function make_shirt(size = "large", text = "I love JavaScript") {
+    console.log(`The size of the shirt is ${size} and printed text is ${text}`)
+}
+make_shirt()
+make_shirt("large");
+make_shirt("medium");
+make_shirt("small", "I love aws");
