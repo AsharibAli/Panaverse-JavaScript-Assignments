d4a5d927378cbd2916cd37825743b8d33db164f6
Asharib Ali <102221198+AsharibAli@users.noreply.github.com>
2022-10-04 11:22:06 -0700

    Task completed
    Pizzas: Think of at least three kinds of your favorite pizza. Store these pizza names in a array, and then use a for loop to print the name of each pizza.
    • Modify your for loop to print a sentence using the name of the pizza instead of printing just the name of the pizza. For each pizza you should have one line of output containing a simple statement like I like pepperoni pizza.
    • Add a line at the end of your program, outside the for loop, that states how much you like pizza. The output should consist of three or more lines about the kinds of pizza you like and then an additional sentence, such as I really love pizza!

---
+++ pana-34.js
@@ -0,0 +1,12 @@
+let pizza_flavors = ["pepperoni", "malaiboti", "Cheese"]
+
+pizza_flavors.forEach((flav) => {
+    console.log(`I like ${flav} pizza`)
+})
+
+console.log(`
+Pizza is a dish of Italian origin consisting of a usually round, flat base of leavened wheat-based dough topped with tomatoes,
+cheese, and often various other ingredients,
+which is then baked at a high`
+)
+console.log(`I really love pizza!`);
