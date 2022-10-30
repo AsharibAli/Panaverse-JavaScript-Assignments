0e20a83020f2fcb2646d3f101cf277929da827fa
Asharib Ali <102221198+AsharibAli@users.noreply.github.com>
2022-10-29 22:33:40 -0700

    Update pana-6.js

--- pana-6.js
+++ pana-6.js
@@ -1,3 +1,3 @@
-const fullName = "\t \t \t Asharib \n Ali \t \t \t";
-console.log(fullName);
-console.log(fullName.trim());
+const spaceName = "\t \t \t Asharib \n Ali \t \t \t";
+console.log(spaceName);
+console.log(spaceName.trim());
