d4c5880d1b5addc625aa01dd32396b420ac9b299
Asharib Ali <102221198+AsharibAli@users.noreply.github.com>
2022-10-29 22:24:30 -0700

    Update pana-5.js

--- pana-5.js
+++ pana-5.js
@@ -1,5 +1,5 @@
-var famous_person = `Albert Einstein`;
-var quote = `“A person who never made a mistake never tried anything new.”`;
-var message = `${famous_person} once said, ${quote}`;
+var famous_person = `Albeert Einstein`;
+var famous_Quote = `"A person who never made a mistake never tried anything new"`;
+var message = `${famous_person} once said, ${famous_Quote}`;
 
 console.log(message);
