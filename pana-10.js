538c3cfe9b49e014f93293aa575cc23f0c08f1f0
Asharib Ali <102221198+AsharibAli@users.noreply.github.com>
2022-10-29 23:03:19 -0700

    Update pana-10.js

--- pana-10.js
+++ pana-10.js
@@ -1,10 +1,12 @@
-//Store a persons name in a variable and print a message to that person.let
+// Store a persons name in a variable and print a message to that person
 
 fullName = "Asharib Ali"
-console.log(`Hello ${fullName}, would you like to learn some Python today?`);
+console.log(`Hello ${fullName}, would you like to learn some JavaScript Today?`);
 
 // Famous Quote by Albert Einstein
-const quote = `"A person who never made a mistake never tried anything new."`;
-const authorName = `Albert Einstein`;
+const author = `Albert Einstein`;
+const quote = `"A person who never made a mistake did not tried anything new"`;
 
-console.log(`${authorName} once said, ${quote}`);
+console.log(`${author} once said, ${quote}`);
+
+`This tasks shows us how we can print a message who wrote that message`;
