9e395d78dada215e095fc4ba85f4bd599b106188
Asharib Ali <102221198+AsharibAli@users.noreply.github.com>
2022-10-31 06:55:52 -0700

    Task Completed
    Greetings: Start with the array you used in Exercise 11, but instead of just printing each person’s name, print a message to them. The text of each message should be the same, but each message should be personalized with the person’s name.

--- pana-12.js
+++ pana-12.js
@@ -1,5 +1,5 @@
-var names = ["Awais", "Shahnoor", "Samad"];
+let names = ["Ali", "Shahnoor", "Samad"];
 
 for(var name of names){
-    console.log(`${name} is my best friend since my school journey`)
-};
+    console.log(`${name} is my best friend since my school journey`);
+}
